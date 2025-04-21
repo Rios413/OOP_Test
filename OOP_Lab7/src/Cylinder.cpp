16435ca394d9cf98c7ae227a6765eeed00b12d76
Rios413 <ramlee8061@gmail.com>
2025-04-21 20:49:48 +0800

    Finish Cylinder.cpp

---
+++ OOP_Lab7/src/Cylinder.cpp
@@ -0,0 +1,38 @@
+# ifndef CYLINDER_CPP
+# define CYLINDER_CPP
+# include "Cylinder.h"
+#include <cmath>
+#include <iomanip>
+
+
+double Cylinder :: SurfaceArea(){
+    
+    return M_PI * radius * 2 * (height + radius);
+}
+
+double Cylinder :: Volume(){
+
+    return M_PI * radius * radius * height;
+}
+
+double Cylinder :: Circumference(){
+    
+    return M_PI * 2 * radius;
+}
+
+istream & operator>>(istream & in, Cylinder & cldr)
+{
+    in >>  cldr.radius >> cldr.height;
+    return in;
+}
+
+ostream & operator<<(ostream & out, Cylinder & cldr)
+{
+    ;
+    out << "Circumference: " << fixed << std::setprecision(3) << cldr.Circumference() << endl;
+    out << "SurfaceArea: " << cldr.SurfaceArea() << endl;
+    out << "Volume: " << cldr.Volume();
+    return out;
+}
+
+# endif
